6deb0b5d6c234ef0551d789e29425dea84e53a14
hclpandv <vikas110@gmail.com>
2022-05-11 05:31:01 +0800

    testinh http provider

--- rg.tf
+++ rg.tf
@@ -1,5 +1,15 @@
 #----Creating a resourceGroup for network resources
-resource "azurerm_resource_group" "rgnetwork" {
-  name     = var.rg_network
-  location = var.location
+# resource "azurerm_resource_group" "rgnetwork" {
+#   name     = var.rg_network
+#   location = var.location
+# }
+
+
+data "http" "example" {
+  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
+
+  # Optional request headers
+  request_headers = {
+    Accept = "application/json"
+  }
 }
